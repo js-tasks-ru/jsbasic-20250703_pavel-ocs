cbf6ca7053d90a5300c8e736ae46336a7762187d
Pavel Zhabotinskiy <89362914+ZhPavelA@users.noreply.github.com>
2025-07-20 01:29:58 +0300

    Решение задачи. Первый заглавный символ

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,17 @@
 function ucFirst(str) {
-  // ваш код...
+    if (str) {
+        let newStr = '';
+        let charStr = '';
+        for (s = 0; s < str.length; s++) {
+            if (s == 0) {
+                charStr = str[s].toUpperCase();
+            } else {
+                charStr = str[s];
+            }
+            newStr += charStr;
+        }
+        return newStr;
+    } else {
+        return str;
+    }
 }
